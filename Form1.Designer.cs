3d497a589c58a3febbc23e8b360fc069c13ae173
Denis <69689773+Denis-Dev-2020@users.noreply.github.com>
2022-02-21 15:42:01 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System.Drawing;
+namespace PDFReaderImages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::PDFReaderImages.Properties.Resources.barRight;
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::PDFReaderImages.Properties.Resources.barLeft;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using PDFReaderImages.Properties;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SautinSoft;
+using iTextSharp;
+//using iTextSharp.text;
+using iTextSharp.text.pdf;
+namespace PDFReaderImages
+{
+    public partial class Form1 : Form
+    {
+        bool Dragging = false;   //  <---- IS IM PRESSING ON AN IMAGE
+        int NewImagesCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();    
+            string pdfPath = "";
+            string BackgroundImagePath = "";
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pdfPath = op.FileName;
+            }
+            string jpegDir = Path.GetDirectoryName(pdfPath);
+            f.OpenPdf(pdfPath);
+            if (f.PageCount > 0)
+            {
+                f.ImageOptions.ImageFormat = ImageFormat.Jpeg;
+                f.ImageOptions.Dpi = 120;
+                f.ImageOptions.JpegQuality = 95;
+                for (int page = 1; page <= f.PageCount- f.PageCount+1; page++)
+                {
+                    string jpegPath = Path.Combine(jpegDir, String.Format("page{0}.jpg", page));
+                    BackgroundImagePath = jpegPath;
+                    f.ToImage(jpegPath, page);
+                }
+            }
+            this.pictureBox2.Image = Image.FromFile(BackgroundImagePath);   //  <--- Outputs image , from PDF convert to IMG
+
+
+
+            /////////////////////////////////   INITIALIZE BACKGROUND PDF TO BE WORKING ON   ////////////////////////////////////////////
+        }
+
+
+
+
+        //   CREATE NEW IMAGE INSTANCE ON YOUR MOUSE POSITION AND IF YOU MOVE MOUSE PIC WILL MOVE TOO UNTIL MOUSECLICK   //
+        private void CreateNewInstance_BasedOnMousePos(String PicPath)
+        {
+            this.Dragging = true;
+            Console.WriteLine(PicPath);
+            Point Point_test = new Point(200, 200);
+            var TestPic = new PictureBox
+            {
+                Name = "pictureBox",
+                Size = new Size(180, 180),
+                Location = new Point(MousePosition.X-206, MousePosition.Y-206),
+                BackColor = Color.White,
+                Image = Image.FromFile(PicPath)
+            };
+            TestPic.MouseDown += new System.Windows.Forms.MouseEventHandler(TestPic_MouseDown);
+            TestPic.MouseMove += new System.Windows.Forms.MouseEventHandler(TestPic_MouseMove);
+            TestPic.MouseUp += new System.Windows.Forms.MouseEventHandler(TestPic_MouseUp);
+            this.Controls.Add(TestPic);
+            TestPic.BringToFront();
+            this.NewImagesCount = NewImagesCount + 1;
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////    HOW ADDED PICTURES ARE GONNA REACT WHEN DIFFERENT EVENTS ACCURE    ///////////
+        public void TestPic_MouseDown(object sender, EventArgs e)
+        {
+            Console.WriteLine("down");
+            if (this.Dragging == true)
+            {
+                this.Dragging = false;
+            } else
+            {
+                this.Dragging = true;
+            }
+        }
+        public void TestPic_MouseUp(object sender, EventArgs e)
+        {
+            Console.WriteLine("up");
+        }
+        public void TestPic_MouseMove(object sender, EventArgs e)
+        {
+            Control c = sender as Control;
+            if (this.Dragging && c != null)
+            {
+                c.Top = MousePosition.Y-204;
+                c.Left = MousePosition.X-204;
+            }
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        private void button2_Click(object sender, EventArgs e)
+        {
+               OpenFileDialog op = new OpenFileDialog();
+               if (op.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+               {
+                CreateNewInstance_BasedOnMousePos(op.FileName);
+               }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            using (var bmp = new Bitmap(400, 400))
+            {
+                pictureBox2.DrawToBitmap(bmp,new Rectangle(0,0,bmp.Width,bmp.Height));
+                bmp.Save("C:\\Users\\drabotay\\OneDrive - Magicsoftware\\Desktop\\Test\\Testabc.jpg");
+            }
+            this.Show();
+        }
+    }
+}
